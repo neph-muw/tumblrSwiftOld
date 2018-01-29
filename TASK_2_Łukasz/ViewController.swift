37a6b24c8d2068e0bdd0441f054b2ac3d25ef0fb
Roman Mykitchak <deviphone@i.ua>
2018-01-29 13:27:54 +0100

    Initial Commit

---
+++ TASK_2_Łukasz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TASK_2_Łukasz
+//
+//  Created by Roman Mykitchak on 1/29/18.
+//  Copyright © 2018 Roman Mykitchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
